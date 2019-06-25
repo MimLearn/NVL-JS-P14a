23287f3b62b5df94021a8cd9622e35aae3680a82
MimLearn <51676988+MimLearn@users.noreply.github.com>
2019-06-25 16:47:22 +0200

    p14a.js

---
+++ p14.js
@@ -0,0 +1,109 @@
+<script>
+/*Rellena una lista con 20-30 películas.
+Nuestro objetivo es recorrer el listado de películas y 
+crear una lista html desordenada donde dependiendo del género pondremos el icono característico del género que estemos tratando.*/
+
+var pelis [
+ {
+   "Nombre": "El señor de los anillos",
+   "Genero": "Acción"
+ },
+ {
+   "Nombre": "Regreso al Futuro",
+   "Genero": "Acción"
+ },
+ {
+   "Nombre": "Los increibles 2",
+   "Genero": "Animación"
+ },
+ {
+   "Nombre": "León",
+   "Genero": "Acción"
+ },
+ {
+   "Nombre": "Inception",
+   "Genero": "Acción"
+ },
+ {
+   "Nombre": "Momo",
+   "Genero": "Animación"
+ },
+ {
+   "Nombre": "El señor de los anillos",
+   "Genero": "Acción"
+ },
+ {
+   "Nombre": "Regreso al Futuro",
+   "Genero": "Acción"
+ },
+ {
+   "Nombre": "Los increibles 2",
+   "Genero": "Animación"
+ },
+ {
+   "Nombre": "León",
+   "Genero": "Acción"
+ },
+ {
+   "Nombre": "Inception",
+   "Genero": "Acción"
+ },
+ {
+   "Nombre": "Momo",
+   "Genero": "Animación"
+ },
+ {
+   "Nombre": "Un funeral de muerte",
+   "Genero": "Comedia"
+ },
+ {
+   "Nombre": "Spiderman",
+   "Genero": "Acción"
+ },
+ {
+   "Nombre": "Spiderman 2",
+   "Genero": "Animación"
+ },
+ {
+   "Nombre": "Spiderman 3",
+   "Genero": "Acción"
+ },
+ {
+   "Nombre": "Kill Bill Vol.1",
+   "Genero": "Acción"
+ },
+ {
+   "Nombre": "Deep blue",
+   "Genero": "Animación"
+ },
+ {
+   "Nombre": "Kill Bill Vol.2",
+   "Genero": "Acción"
+ },
+ {
+   "Nombre": "Taxi Driver",
+   "Genero": "Acción"
+ },
+ {
+   "Nombre": "Totoro",
+   "Genero": "Animación"
+ },
+ {
+   "Nombre": "El mito de Bourne",
+   "Genero": "Acción"
+ },
+ {
+   "Nombre": "Catwoman",
+   "Genero": "Acción"
+ },
+ {
+   "Nombre": "El castillo ambulante",
+   "Genero": "Animación"
+ },
+
+]
+
+
+
+
+</script>
